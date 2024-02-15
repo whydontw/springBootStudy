870dfb1701502591e69103638d1dc7911b10d10f
kimhyejin <kimhyejin@DESKTOP-JBQLC8K>
2024-02-15 12:31:41 +0900

    first commit

---
+++ springBoot/src/main/java/com/boot/spring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.boot.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("hello")
+	public String hello(Model model) {
+		model.addAttribute("data", "Hello Spring!");
+		return "hello";
+	}
+	
+	
+}
